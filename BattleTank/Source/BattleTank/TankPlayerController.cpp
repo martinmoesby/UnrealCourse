fcd7102e1398dff5144be477fc605336b440185b
WORKSTATION\Martin <martinmoesby@martinmoesby.com>
2018-03-27 00:12:56 +0200

    Created PlauerController class and Tank class. Used as base classes for TankController_BP and Tank_BPs

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+
+	Super::BeginPlay();
+
+	ATank* ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("PlayerController NOT controlling a Tank"));
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller controlling pawn: %s"), *(ControlledTank->GetName()));
+
+
+}
